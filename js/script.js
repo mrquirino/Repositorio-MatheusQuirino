d34305466927984adeae723b910dd264579259d3
Matheus Quirino <matheusf730@gmail.com>
2024-02-05 19:42:54 -0300

    parte7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 /*========== scroll selections active link =============*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let heigth = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + heigth) {
+            navLinks.forEach(links => {
+                links.classlist.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+/*========== sticky navbar =============*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+    
+};
