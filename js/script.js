5e1156b869c2765c9ec5c2c244849376edfdfd8b
Matheus Quirino <matheusf730@gmail.com>
2024-02-07 21:21:38 -0300

    js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ window.onscroll = () => {
 
         if (top >= offset && top < offset + heigth) {
             navLinks.forEach(links => {
-                links.classlist.remove('active');
+                links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
