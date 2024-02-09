96c7b0a566c5df46ca86623dd6d95d4838809cae
Matheus Quirino <matheusf730@gmail.com>
2024-02-08 22:39:26 -0300

    fim
    finalização do cod, indo para modificações.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
+}
+
 /*========== scroll selections active link =============*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -25,4 +31,29 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
+     /*========== remove toggle icon and navbar when click navbar link ( scroll ) =============*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+ /*========== scroll reverse =============*/
+ ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'rigth' });
+
+/*========== typed js =============*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Gamer Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
